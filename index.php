d33dbe1bf3a1a41ea6fd118fa267894782d57e18
Rizal M noer <rizal.muhamad@student.unsil.ac.id>
2014-05-05 07:29:43 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>TUGAS BESAR 1 WEBSERVICE</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="konten">
+ <div id="head">
+  <div id="logo">
+  </div>
+   <div id="nav">
+    <ul class="nav-link">
+       <li id="acitve-nav"><a href="index.php" title="home">home</a></li>
+       <li><a href="about.php" title="about">about</a></li>
+       <li class="border-right"><a href="contact.php" title="contact">contact</a></li>
+    </ul>
+   </div>
+ </div> 
+
+ <div id="welcome">
+  <div id="welcome-text">
+  <h2>welcome</h2>
+  <p>Terimakasih sudah bersedia berkunjung ke website kami.
+  Di website ini kita telah menyajikan berbagai informasi yang bisa menambah wawasan anda mengenai:
+  <br>
+  <br>
+  <ul>
+  <li><i>Berita Pemerintahaan di indonesia</i>
+  <li><i>Bertia Pendidikaan di indonesia</i>
+  <li><i>Berita Sepakbola terupdate</i>
+  <li><i>Berita Selebritis indonesia</i>
+  </ul>
+  <br>
+  <h3>Berita Pemerintahan di indonesia</h3>
+    <p> Pertama berita yang akan di sajikan berita pemerintahaan di indonesia kita harus mengetahui apa saja yang di lakukan oleh pemerintah untuk negara kita tercinta ini
+	   untuk mengetahui nya silahkan <a href="Berita_Pemerintahaan.php">Kunjungi</a>.</p>
+  </div>
+   <div id="welcome-image">  
+   <img src="img/pemerintah.jpg" align="right" width="300" height="400" />
+  </div>
+ </div>  
+ <div class="line">&nbsp;</div>
+ <div id="conten">
+  <div id="conten-text">
+  <h3>BERITA PENDIDIKAN DI INDONESIA</h3>
+  <p>Berita yang ke dua yang kita sajikan berita pendidikan di indonesia,pendidikan itu sanangat
+  penting untuk kita dan untuk memajukan negara tercinta ini,maka dari itu silahkan kunjungi informasi mengenai pendidikaan 
+     atau yang menyelengarakan tentang pendidikan di indonesia<a href="Berita_Pendidikaan.php">Kunjungi</a></p>  
+  <img src="img/pendidikan.jpg" align="center" width="600" height="200" />
+ <div class="bottom">&nbsp;</div>
+ <div id="conten">
+  <div id="conten-text">
+  <h3>Berita Sepakbola terupdate</h3>
+  <p>Ini pas banget buat anda-anda yang suka olahraga dan ingin mengetahui berita-berita sepakbola luar ataupun dalam negri yang terupdate,untuk lebih jelas sialhkan<a href="Berita_Bola.php">Kunjungi</a><img src="img/bola.jpg" align="right" width="200" height="200" />  
+  <div class="bottom">&nbsp;</div>
+ <div id="conten">
+  <div id="conten-text">
+  <h3>Berita Selebritis</h3>
+  <p>Biasanya berita selebritis lebih enak menyimak nya sambil santai-santai dan minum teh hangat di tambah sedikit cemilan yang memanjakan lidah anda,apapun makanan nya minum nya terserah anda,untuk lebih mempersingkat waktu silahkan<a href="Berita_Seleb.php">Kunjungi</a>  
+  <div class="bottom">&nbsp;</div>
+ <div id="footer">
+  <div id="footer-link">
+  <a href="index.php">home</a> | <a href="about.php">about</a> | <a href="contact.php">contact</a>  
+  </div>
+  <div id="footer-copy">
+  <p>copyright Upi latipudin | Helmy Pratama Tryas Rustandy | Wandi ilham | Rizal muhamad noor </p>
+  </div>
+ </div>
+</div>
+</body>
+</html>
